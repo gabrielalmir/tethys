23759082822118978a5bf6a4cb6ecb43387e6520
gabrielalmir <gabr.almir@gmail.com>
2023-11-18 12:39:15 -0300

    feat: add basic auth to api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 import { z } from "zod";
 
+import bcrypt from 'bcrypt';
+
 const app = fastify();
 const prisma = new PrismaClient();
 
@@ -10,17 +12,49 @@ app.get("/users", async () => {
   return { users };
 });
 
-app.post("/users", async (request, reply) => {
+app.post("/login", async (request, reply) => {
+  const createSessionSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = createSessionSchema.parse(request.body);
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return reply.status(404).send({ error: "Usuário não encontrado" });
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    return reply.status(400).send({ error: "Usuário ou senha não correspondem" });
+  }
+
+  const { password: _, ...userData } = user;
+
+  return reply.status(200).send({ userData });
+});
+
+app.post("/register", async (request, reply) => {
   const createUserSchema = z.object({
     name: z.string(),
     email: z.string().email(),
+    password: z.string().min(6),
   });
 
-  const { name, email } = createUserSchema.parse(request.body);
+  const { name, email, password } = createUserSchema.parse(request.body);
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   await prisma.user.create({
     data: {
       name,
       email,
+      password: hashedPassword
     },
   });
 
