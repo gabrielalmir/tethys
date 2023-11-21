e3455f6462fc562b34f01696aec0aed0bdbd2c35
Gabriel Almir <gabr.almir@gmail.com>
2023-11-21 23:31:15 +0000

    feat: add postal code and phone number in register

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const baseURL = 'https://tethys-api-auth.onrender.com'
-const $registerForm = document.getElementById('register');
-const $loginForm = document.getElementById('login');
+const baseURL = "https://tethys-api-auth.onrender.com";
+const $registerForm = document.getElementById("register");
+const $loginForm = document.getElementById("login");
 
 function main() {
     const currentPath = window.location.pathname;
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
 
     if (
         user &&
@@ -13,19 +13,19 @@ function main() {
         window.location.href = "dashboard.html";
     }
 
-    if (!user && currentPath === '/dashboard.html') {
+    if (!user && currentPath === "/dashboard.html") {
         window.location.href = "login.html";
     }
 
     if ($registerForm) {
-        $registerForm.addEventListener('submit', (e) => {
+        $registerForm.addEventListener("submit", (e) => {
             e.preventDefault();
             register(e);
         });
     }
 
     if ($loginForm) {
-        $loginForm.addEventListener('submit', (e) => {
+        $loginForm.addEventListener("submit", (e) => {
             e.preventDefault();
             login(e);
         });
@@ -36,24 +36,24 @@ function login(e) {
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    console.log('Logging in...');
+    console.log("Logging in...");
     console.log(email, password);
 
     fetch(`${baseURL}/login`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({ email: email, password }),
         headers: {
-            'Content-Type': 'application/json'
-        }
+            "Content-Type": "application/json",
+        },
     })
-        .then(res => res.json())
-        .then(user => {
+        .then((res) => res.json())
+        .then((user) => {
             if (user) {
-                localStorage.setItem('user', JSON.stringify(user));
+                localStorage.setItem("user", JSON.stringify(user));
                 window.location.href = "dashboard.html";
             }
         })
-        .catch(err => console.error(err));
+        .catch((err) => console.error(err));
 }
 
 function register(e) {
@@ -62,22 +62,27 @@ function register(e) {
     const cep = e.target.cep.value;
     const phone = e.target.phone.value;
 
-    console.log('Registering...');
+    console.log("Registering...");
     console.log(email, password, cep, phone);
 
     fetch(`${baseURL}/register`, {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
+        method: "POST",
+        body: JSON.stringify({
+            email,
+            password,
+            postalCode: cep,
+            phoneNumber: phone,
+        }),
         headers: {
-            'Content-Type': 'application/json'
-        }
+            "Content-Type": "application/json",
+        },
     })
-        .then(res => {
+        .then((res) => {
             if (res.status === 201) {
-                window.location.href = "login.html";
+                window.location.href = "dashboard.html"
             }
         })
-        .catch(err => console.error(err));
+        .catch((err) => console.error(err));
 }
 
 main();
