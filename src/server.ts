0a9755d430ee0197522d9ce8d5d15266deaa7710
gabrielalmir <gabr.almir@gmail.com>
2023-11-24 17:49:07 -0300

    feat: add user notifier

--- src/server.ts
+++ src/server.ts
@@ -1,71 +1,32 @@
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 import cron from "node-cron";
+
 import { BrasilApiService } from "./services/brasil-api";
 import { SmsService } from "./services/sms-api";
+import { UserNotifier } from "./services/user-notifier";
 import { WeatherService } from "./services/weather.api";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
-cron.schedule('*/10 * * * * *', async () => {
-  const users = await prisma.user.findMany()
+const userNotifier = new UserNotifier(
+  new BrasilApiService(fetch),
+  new WeatherService(fetch),
+  new SmsService(fetch),
+  new PrismaClient()
+);
 
-  console.log(`Enviando SMS para ${users.length} usuários`)
+const EVERY_DAY = "0 0 * * *";
 
-  users.map(async (user) => {
-    try {
-      validateUser(user);
-
-      const { postalCode, phoneNumber } = user;
-
-      console.log(`Buscando dados para o CEP ${postalCode}`)
-
-      const brasilApiService = new BrasilApiService(fetch);
-      const { latitude, longitude } = await brasilApiService.getPostalCode(postalCode);
-
-      if (!latitude || !longitude) {
-        throw new Error("Não foi possível encontrar as coordenadas do CEP");
-      }
-
-      console.log(`Buscando dados de chuva para latitude ${latitude} e longitude ${longitude}`)
-
-      const weatherService = new WeatherService(fetch);
-      const { rainfall } = await weatherService.getRainfall(
-        latitude,
-        longitude
-      );
-
-      if (rainfall !== 0 && !rainfall) {
-        console.log(await weatherService.getRainfall(latitude, longitude))
-        throw new Error("Não foi possível encontrar o índice de chuva");
-      }
-
-      console.log(`Enviando SMS para ${phoneNumber}`)
-
-      const smsService = new SmsService(fetch);
-      await smsService.sendSms(phoneNumber, `Olá! O índice de chuva na sua região é de ${rainfall} mm`);
-
-      console.log(`SMS enviado para ${phoneNumber}`);
-    } catch (error: any) {
-      console.log(error.message);
-    }
-  })
+cron.schedule(EVERY_DAY, async () => {
+  console.log("Iniciando notificação de usuários");
+  await userNotifier.notifyUsers();
 });
 
-function validateUser(user: User) {
-  if (!user) {
-    throw new Error("Usuário não encontrado");
-  }
-
-  if (!user.postalCode) {
-    throw new Error("CEP não encontrado");
-  }
-
-  if (!user.phoneNumber) {
-    throw new Error("Número de telefone não encontrado");
-  }
-}
+app.post('/notify', async (request, reply) => {
+  await userNotifier.notifyUsers()
+  reply.send('ok')
+})
 
 app
   .listen({
---
+++ src/services/user-notifier.ts
@@ -0,0 +1,80 @@
+import { PrismaClient, User } from "@prisma/client";
+import { BrasilApiService } from "./brasil-api";
+import { SmsService } from "./sms-api";
+import { WeatherService } from "./weather.api";
+
+export class UserNotifier {
+  constructor(
+    private brasilApiService: BrasilApiService,
+    private weatherService: WeatherService,
+    private smsService: SmsService,
+    private prisma: PrismaClient
+  ) {}
+
+  async notifyUsers() {
+    const users = await this.prisma.user.findMany();
+
+    console.log(`Enviando SMS para ${users.length} usuários`);
+
+    users.map(async (user) => {
+      await this.notifyUser(user);
+    });
+  }
+
+  private async notifyUser(user: User) {
+    try {
+      this.validateUser(user);
+
+      const { postalCode, phoneNumber } = user;
+
+      console.log(`Buscando dados para o CEP ${postalCode}`);
+
+      const { latitude, longitude } = await this.brasilApiService.getPostalCode(
+        postalCode
+      );
+
+      if (!latitude || !longitude) {
+        throw new Error("Não foi possível encontrar as coordenadas do CEP");
+      }
+
+      console.log(
+        `Buscando dados de chuva para latitude ${latitude} e longitude ${longitude}`
+      );
+
+      const { rainfall } = await this.weatherService.getRainfall(
+        latitude,
+        longitude
+      );
+
+      if (rainfall !== 0 && !rainfall) {
+        console.log(await this.weatherService.getRainfall(latitude, longitude));
+        throw new Error("Não foi possível encontrar o índice de chuva");
+      }
+
+      console.log(`Enviando SMS para ${phoneNumber}`);
+
+      await this.smsService.sendSms(
+        phoneNumber,
+        `Olá! O índice de chuva na sua região é de ${rainfall} mm`
+      );
+
+      console.log(`SMS enviado para ${phoneNumber}`);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  private validateUser(user: User) {
+    if (!user) {
+      throw new Error("Usuário não encontrado");
+    }
+
+    if (!user.postalCode) {
+      throw new Error("CEP não encontrado");
+    }
+
+    if (!user.phoneNumber) {
+      throw new Error("Número de telefone não encontrado");
+    }
+  }
+}
