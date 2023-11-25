ecff273b7fbdec7472db8159713a5296ca05b211
gabrielalmir <gabr.almir@gmail.com>
2023-11-25 17:43:23 -0300

    feat: add notify users command

---
+++ app/Console/Commands/NotifyUsersCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class NotifyUsersCommand extends Command
+{
+    private $baseUrl = 'https://task-alerta-alagamento.onrender.com';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:notify-users-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Notify users about the flood alert.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Notifying users...');
+
+        // Get all users
+        $users = User::all();
+
+        if ($users->isEmpty()) {
+            $this->info('No users found.');
+
+            return;
+        }
+
+        // Notify each user
+        $users->each(function (User $user) {
+            $this->info("Notifying user {$user->email}...");
+
+            $this->notifyUser($user);
+        });
+    }
+
+    /**
+     * Notify user.
+     *
+     * @param User $user
+     */
+    private function notifyUser(User $user)
+    {
+        $url = "{$this->baseUrl}/notify";
+
+        Http::async()->post($url, [
+            'email' => $user->email,
+        ]);
+
+        $this->info("User {$user->email} notified.");
+    }
+}
