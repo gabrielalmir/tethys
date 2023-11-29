04324075a922d3a55be295572604abe4a2659b2a
Gabriel Almir <gabr.almir@gmail.com>
2023-11-29 22:51:53 +0000

    feat: fetch database mongo

--- app/Filament/Widgets/RainfallNotificationChart.php
+++ app/Filament/Widgets/RainfallNotificationChart.php
@@ -3,11 +3,21 @@
 namespace App\Filament\Widgets;
 
 use Filament\Widgets\ChartWidget;
+use MongoDB\Driver\ServerApi;
 
 class RainfallNotificationChart extends ChartWidget
 {
     protected static ?string $heading = 'Gráfico de chuvas e volume do lago';
     protected static string $color = 'info';
+
+    private MongoDB\Client $mongoClient;
+
+    public function __construct() {
+        $mongoUri = getenv('MONGODB_URI', 'mongodb://localhost:27017');
+        $mongoApi = new ServerApi(ServerApi::V1);
+        $mongoClient = new MongoDB\Client($mongoUri, [], ['serverApi' => $mongoApi]);
+        $this->mongoClient = $mongoClient;
+    }
 
     protected static ?array $options = [
         'animations' => [
@@ -21,64 +31,26 @@ class RainfallNotificationChart extends ChartWidget
         ],
     ];
 
+    private function fetchDataFromDatabase(): array
+    {
+        $collection = $this->mongoClient->selectDatabase('tethys')->selectCollection('notifications');
+        $cursor = $collection->find([], ['_id' => 0, 'rainfall' => 1, 'volume' => 1]);
+
+        $data = [];
+
+        foreach ($cursor as $document) {
+            $data[] = [
+                'rainfall' => $document['rainfall'],
+                'volume' => $document['volume'],
+            ];
+        }
+
+        return $data;
+    }
+
     protected function getData(): array
     {
-        $data = [
-            [
-                'city' => 'Blumenau',
-                'neighborhood' => 'Centro',
-                'postal_code' => '89010025',
-                'state' => 'SC',
-                'time' => '2023-11-25 22:32:13.306000',
-                'rainfall' => 0,
-                'lake_volume' => 17,
-            ],
-            [
-                'city' => 'Itapira',
-                'neighborhood' => 'Centro',
-                'postal_code' => '13970342',
-                'state' => 'SP',
-                'time' => '2023-11-25 22:33:09.814000',
-                'rainfall' => 1,
-                'lake_volume' => 82,
-            ],
-            [
-                'city' => 'Itapira',
-                'neighborhood' => 'Centro',
-                'postal_code' => '13970080',
-                'state' => 'SP',
-                'time' => '2023-11-25 23:27:34.678000',
-                'rainfall' => 1,
-                'lake_volume' => 55,
-            ],
-            [
-                'city' => 'Itapira',
-                'neighborhood' => 'Centro',
-                'postal_code' => '13970080',
-                'state' => 'SP',
-                'time' => '2023-11-25 23:30:17.538000',
-                'rainfall' => 1,
-                'lake_volume' => 65,
-            ],
-            [
-                'city' => 'Blumenau',
-                'neighborhood' => 'Centro',
-                'postal_code' => '89010025',
-                'state' => 'SC',
-                'time' => '2023-11-26 15:55:41.368000',
-                'rainfall' => 0,
-                'lake_volume' => 6,
-            ],
-            [
-                'city' => 'Blumenau',
-                'neighborhood' => 'Centro',
-                'postal_code' => '89010025',
-                'state' => 'SC',
-                'time' => '2023-11-26 18:02:01.334000',
-                'rainfall' => 0,
-                'lake_volume' => 63,
-            ]
-        ];
+        $data = $this->fetchDataFromDatabase() ?? [];
 
         return [
             'datasets' => [
