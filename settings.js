38162afbc56fc8981dadb564ddf8a29ba7d0efdb
gabrielalmir <gabr.almir@gmail.com>
2023-12-02 23:20:30 -0300

    feat: add cron

--- settings.js
+++ settings.js
@@ -73,14 +73,14 @@ module.exports = {
     /** To password protect the Node-RED editor and admin API, the following
      * property can be used. See https://nodered.org/docs/security.html for details.
      */
-    adminAuth: {
-       type: "credentials",
-       users: [{
-           username: "admin",
-           password: process.env.NODERED_PASSWORD,
-           permissions: "*"
-       }]
-    },
+    //adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: "admin",
+    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+    //        permissions: "*"
+    //    }]
+    //},
 
     /** The following property can be used to enable HTTPS
      * This property can be either an object, containing both a (private) key
