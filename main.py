6af92eeed6c6ab1c19716484bc66a4cc6b47d9da
gabrielalmir <gabr.almir@gmail.com>
2023-12-06 06:05:29 -0300

    fix: message error without country code

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ app = FastAPI(debug=True)
 
 load_dotenv()
 
+
 @app.post("/enviar-dados")
 async def enviar_dados(numero: str = Form(...), texto: str = Form(...)):
     # Enviar SMS
     message_sid = enviar_sms(numero, texto)
 
-    return {"message_sid": message_sid}
+    return message_sid
+
 
 def enviar_sms(numero, texto):
     # Obter credenciais da Twilio do ambiente
-    account_sid = os.getenv('ACCOUNT_SID1')
-    auth_token = os.getenv('AUTH_TOKEN1')
+    account_sid = os.getenv("ACCOUNT_SID1")
+    auth_token = os.getenv("AUTH_TOKEN1")
 
     # Criar cliente Twilio
     client = Client(account_sid, auth_token)
 
     # Números de telefone
-    from_phone_number = os.getenv('PHONE_NUMBER', '+18452432674')
-    to_phone_number = numero
+    from_phone_number = os.getenv("PHONE_NUMBER", "+18452432674")
+    to_phone_number = numero if "+55" in numero else f"+55{numero}"
 
     # Corpo da mensagem
     message_body = texto
@@ -38,21 +40,28 @@ def enviar_sms(numero, texto):
         try:
             # Enviar mensagem
             message = client.messages.create(
-                body=message_body,
-                from_=from_phone_number,
-                to=to_phone_number
+                body=message_body, from_=from_phone_number, to=to_phone_number
             )
 
             # Imprimir informações sobre o envio bem-sucedido
             print(f"SMS enviado com sucesso, SID: {message.sid}")
 
-            # Retorna o SID da mensagem na resposta da API
-            return {"message_sid": message.sid}
+            # Retorna a mensagem
+            return {
+                "message": "SMS enviado com sucesso",
+                "sid": message.sid,
+                "status": message.status,
+                "to": message.to,
+                "from": message.from_,
+                "body": message.body,
+            }
 
         except TwilioRestException as e:
             # Lidar com exceções da Twilio
             if e.code == 21211:
-                print(f"Não foi possível realizar a operação. Número de telefone inválido.")
+                print(
+                    f"Não foi possível realizar a operação. Número de telefone inválido."
+                )
             else:
                 print(f"Erro ao enviar o SMS: {e.msg}")
             return {"error": "Erro ao enviar o SMS"}
@@ -61,6 +70,3 @@ def enviar_sms(numero, texto):
         # Se o corpo da mensagem estiver vazio
         print("O corpo da mensagem está vazio. Não é possível enviar o SMS.")
         return {"error": "O corpo da mensagem está vazio. Não é possível enviar o SMS."}
-
-
-
